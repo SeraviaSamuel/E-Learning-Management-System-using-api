e96cd21a7f0a1dbebad06b21b7a0bb25b261f9db
SeraviaSamuel <seravia.sameh18@gmail.com>
2024-05-02 00:10:15 +0300

    Genric repository

---
+++ E-Learning-Management-System/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace E_Learning_Management_System.Repository
+{
+    public interface IRepository<T> where T : class, IDeletable
+    {
+        public void insert(T obj);
+        public void delete(T obj);
+        public void update(T obj);
+        public ICollection<T> GetAll();
+        public T Get(Func<T, bool> predicate);
+        public int save();
+    }
+}
